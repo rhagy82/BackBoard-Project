29173042191674b49ba7de621e1765f318fd30e1
rhagy82 <71342149+rhagy82@users.noreply.github.com>
2021-02-09 13:18:38 -0500

    Create psuedocode.ino

---
+++ psuedocode.ino
@@ -0,0 +1,27 @@
+
+
+void setup()
+{
+// attach arduino to the sensor and liquid display
+}
+
+void loop()
+{
+  //read the distance of the ball
+
+  //If the person makes the basket add points
+
+        // If the basket is good add points 
+            //If the person misses don't add points
+            //tell the arduino to track makes and misses using distance
+            //tell led to add points
+            
+
+  //Contine the loop everytime the ball is sensed
+
+  //We need to serial print the values
+
+  // 17inches wide 12/1/2 inches tall rim 9 inches in diameter 3/1/2 inches from the bottom
+
+}
+
